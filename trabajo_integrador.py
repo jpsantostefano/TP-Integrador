7ad23bfa9333f0843115d92d8aa9a5bb56f3129a
jpsantostefano <jpsantostefano@gmail.com>
2025-09-19 16:31:03 -0300

    Opcion AND hecha y testeada

---
+++ trabajo_integrador.py
@@ -0,0 +1,33 @@
+# Simulacion de Puertas Logicas Basicas:
+
+print("PROYECTO MATEMATICA Y PROGRAMACION")
+
+print("Elige que puerta logica deseas usar: ")
+print("1. AND")
+print("2. OR")
+print("3. NOT")
+print("0. Salir")
+opcion = int(input())
+
+if opcion == 1 or opcion == 2:
+    valor1 = int(input("Ingrese el primer valor (0-1): "))
+    valor2 = int(input("Ingrese el segundo valor (0-1): "))
+    if opcion == 1:
+        if valor1 == 1 and valor2 == 1:
+            print(f"La salida de {valor1} y {valor2} es: 1")
+        else:
+            print(f"La salida de {valor1} y {valor2} es: 0")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
